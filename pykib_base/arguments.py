df165d3944e2b7ab1fd755b65856205e0a039140
Tobias Wintrich <info@winteach.de>
2020-01-30 17:32:55 +0100

    increased Version Number

--- pykib_base/arguments.py
+++ pykib_base/arguments.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 import configparser
 import io
 
-__version_info__ = ('devel', '1.0.17')
+__version_info__ = ('devel', '1.0.18')
 __version__ = '-'.join(__version_info__)
 
 
